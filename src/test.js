432873d7f7111232cee937b9031b2edac9c1dd28
soulsam480 <soulsam480@hotmail.com>
2020-08-04 10:11:50 +0530

    test

--- src/test.js
+++ src/test.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const serverless = require('serverless-http')
-
-const app = express()
-const router = express.Router()
-router.get('/',(req,res)=>{
-    res.send(
-        "yoyo"
-    )
-})
-app.use('/.netlify/functions/test',router)
-module.exports.handler = serverless(app)
+const express = require("express");
+const serverless = require("serverless-http");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+const corsOptions = {
+  allowedHeaders: ["Content-Type"],
+};
+const router = express.Router();
+router.get("/", cors(corsOptions), (req, res) => {
+  const name = req.body.name;
+  res.statusCode("200").send(`hello ${name}`);
+});
+app.use("/.netlify/functions/test", router);
+module.exports.handler = serverless(app);
