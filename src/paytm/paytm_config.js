474de94c36732d6c0f7ffa9503d034d6b47ab4c7
soulsam480 <soulsam480@hotmail.com>
2020-08-04 17:40:07 +0530

    testing again

--- src/paytm/paytm_config.js
+++ src/paytm/paytm_config.js
@@ -1,11 +1,14 @@
+const { MERCHANT_KEY } = process.env;
+const { MID } = process.env;
+
 module.exports = {
   paytm_config: {
     PAYTM_ENVIRONMENT: "TEST", //possible values:  TEST | PROD
-    MID: "UKMNIW84275080378144", // Get it From https://dashboard.paytm.com/next/apikeys use Test id for test purpose and Production id for Production Purpose
+    MID: MID, // Get it From https://dashboard.paytm.com/next/apikeys use Test id for test purpose and Production id for Production Purpose
     WEBSITE: "WEBSTAGING", // USE WEBSTAGING for testing, You Will get it for Production here https://dashboard.paytm.com/next/apikeys
     CHANNEL_ID: "WEB", // Use WEB for Desktop Website and WAP for Mobile Website
     INDUSTRY_TYPE_ID: "Retail", // Use Retail for Testing, For Production You Can Get it from here https://dashboard.paytm.com/next/apikeys
-    MERCHANT_KEY: "Fd6yE&J04j@OCQSg", // Get it From https://dashboard.paytm.com/next/apikeys use Test key for test purpose and Production key for Production Purpose
+    MERCHANT_KEY: MERCHANT_KEY, // Get it From https://dashboard.paytm.com/next/apikeys use Test key for test purpose and Production key for Production Purpose
     CALLBACK_URL:
       "https://hopeful-mirzakhani-a59182.netlify.app/.netlify/functions/payConf", // Modify and Use this url for verifying payment, we will use cloud function DonationCallback function for Our usage
 
